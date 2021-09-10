c7d5674f1f22736844678b25ec9d5c3c152da20e
Raj Shekhar <raj.nsit@gmail.com>
2021-09-09 23:51:11 -0500

    Adding standalone encoder in TurboAE

--- channel_ae.py
+++ channel_ae.py
@@ -17,6 +17,25 @@ class Channel_AE(torch.nn.Module):
         self.enc = enc
         self.dec = dec
 
+    def encode(self, input):
+        # Setup Interleavers.
+        if self.args.is_interleave == 0:
+            pass
+
+        elif self.args.is_same_interleaver == 0:
+            interleaver = RandInterlv.RandInterlv(self.args.block_len, np.random.randint(0, 1000))
+
+            p_array = interleaver.p_array
+            self.enc.set_interleaver(p_array)
+
+        else:# self.args.is_same_interleaver == 1
+            interleaver = RandInterlv.RandInterlv(self.args.block_len, 0) # not random anymore!
+            p_array = interleaver.p_array
+            self.enc.set_interleaver(p_array)
+
+        codes  = self.enc(input)
+        return codes
+        
     def forward(self, input, fwd_noise):
         # Setup Interleavers.
         if self.args.is_interleave == 0:
